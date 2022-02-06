b21687951940c1a9392254c768880889b727166c
Amitabh Ranjan <amitabh.march22@gmail.com>
2022-02-06 16:46:56 -0500

    create models/product.js and add name & price to productSchema

---
+++ backend/models/product.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const { stringify } = require('querystring');
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please enter product name'],
+        trim: true,
+        maxLength: [100, 'Product name cannot exceed 100 characters']
+    },
+    price: {
+        type: Number,
+        required: [true, 'Please enter product price'],
+        maxLength: [5, 'Product name cannot exceed 5 characters'],
+        default: 0.0
+    },
+})
+
+module.exports = mongoose.model('product', productSchema);
