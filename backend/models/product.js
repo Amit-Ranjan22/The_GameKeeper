d3c55c5d0224daf929b5950f26251aa9e3e76253
Amitabh Ranjan <amitabh.march22@gmail.com>
2022-02-06 18:14:27 -0500

    add numOfReviews,stock,seller,revirws,createdAt to productSchema in models/product.js

--- backend/models/product.js
+++ backend/models/product.js
@@ -52,6 +52,40 @@ const productSchema = new mongoose.Schema({
             message: 'Please select correct category for product'
         }
     },
+    seller: {
+        type: String,
+        required: [true, 'Please enter product seller']
+    },
+    stock: {
+        type: Number,
+        required: [true, 'Please enter product stock'],
+        maxLength: [5, 'Product name cannot exceed 5 characters'],
+        default: 0
+    },
+    numOfReviews: {
+        type: Number,
+        default: 0
+    },
+    reviews: [
+        {
+            name: {
+                type: String,
+                required: true
+            },
+            rating: {
+                type: Number,
+                required: true
+            },
+            comment: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
 })
 
 module.exports = mongoose.model('product', productSchema);
