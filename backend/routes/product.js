962a61072a327641b959d03560f218cb609b8670
Amitabh Ranjan <amitabh.march22@gmail.com>
2022-02-07 17:30:58 -0500

    write updateProduct() function in productController.js to update a product by id and set its route in routes/product.js

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -2,11 +2,12 @@ const express = require('express')
 const router = express.Router();
 
 //importing getProducts from controllers
-const { getProducts, newProduct, getSingleProduct } = require('../controllers/productController')
+const { getProducts, newProduct, getSingleProduct, updateProduct } = require('../controllers/productController')
 
 //setting route to get all products
 router.route('/products').get(getProducts);
 router.route('/product/:id').get(getSingleProduct);
-router.route('/product/new').post(newProduct);
+router.route('/admin/product/new').post(newProduct);
+router.route('/admin/product/:id').put(updateProduct);
 
 module.exports = router;
