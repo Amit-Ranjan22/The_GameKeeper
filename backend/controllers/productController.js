50d639f75203d04592eda574d7e4bda95e0d14a8
Amitabh Ranjan <amitabh.march22@gmail.com>
2022-02-08 18:36:15 -0500

    create utils/apiFeatures.js and write search functionality to search with a keyword

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -3,6 +3,7 @@ const Product = require('../models/product')
 const ErrorHandler = require('../utils/errorHandler');
 
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors')
+const APIFeatures = require('../utils/apiFeatures')
 
 //controller function to add new product
 // Create new product => /api/v1/admin/product/new
@@ -16,9 +17,13 @@ exports.newProduct = catchAsyncErrors(async (req, res, next) => {
 
 
 //controller function to get all products
-//GEt all products => /api/v1/products
+//GEt all products => /api/v1/products?keyword=apple
 exports.getProducts = catchAsyncErrors(async (req, res, next) => {
-    const products = await Product.find();
+
+    const apiFeatures = new APIFeatures(Product.find(), req.query)
+        .search()
+
+    const products = await apiFeatures.query;
 
     res.status(200).json({
         success: true,
