a432394eabe65ab0f2d0970f70109255b07b8a87
Amitabh Ranjan <amitabh.march22@gmail.com>
2022-02-09 13:09:31 -0500

    add filter method to apiFeature class in uti/apiFeatutes.js to filter the products

--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -16,6 +16,21 @@ class APIFeatures {
         this.query = this.query.find({ ...keyword });
         return this;
     }
+
+    filter() {
+        const queryCopy = {...this.queryStr};
+
+        console.log(queryCopy);
+
+        //removing fields from the query for filtering
+        const removeFields = ['keyword', 'limit', 'page']
+        removeFields.forEach(el => delete queryCopy[el]);
+
+        // console.log(queryCopy);
+        this.query = this.query.find(queryCopy);
+        return this;
+
+    }
 }
 
 module.exports = APIFeatures
