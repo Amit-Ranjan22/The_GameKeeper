c7c6114f7f6b22e67dbbad9f0d87c047f2693424
Amitabh Ranjan <amitabh.march22@gmail.com>
2022-02-15 15:00:29 -0500

    a minor changes in authcontroller and product controller

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,24 @@ const express = require('express');
 const app = express();
 
 const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
 
 const errorMiddleware = require('./middlewares/errors')
 
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(fileUpload());
+
+//setting cloudinary configuration
+// cloudinary.config({
+//     cloud_name : process.env.CLOUDINARY_CLOUD_NAME,
+//     api_key : process.env.CLOUDINARY_API_KEY,
+//     api_secret : process.env.CLOUDINARY_API_SECRET
+// })
+
+
 
 //importing all routes
 const products = require('./routes/product');
--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -24,7 +24,7 @@ exports.newProduct = catchAsyncErrors(async (req, res, next) => {
 //GEt all products => /api/v1/products?keyword=apple
 exports.getProducts = catchAsyncErrors(async (req, res, next) => {
 
-    const resPerPage = 4;
+    const resPerPage = 8;
 
     //"productCount" will be used in the front end to show total number of products on a page.
     const productsCount = await Product.countDocuments();
