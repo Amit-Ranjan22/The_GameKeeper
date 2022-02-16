51a513908b426710c9fb7fcdb83776e3c42c6d0c
Amitabh Ranjan <amitabh.march22@gmail.com>
2022-02-16 11:06:16 -0500

    add the cart component to display the cart items

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -3,7 +3,7 @@ import { Route, Link } from 'react-router-dom'
 
 import { useDispatch, useSelector } from 'react-redux'
 import { useAlert } from 'react-alert'
-import {logout} from '../../actions/userActions'
+import { logout } from '../../actions/userActions'
 
 import Search from './Search'
 
@@ -14,6 +14,7 @@ const Header = () => {
     const dispatch = useDispatch();
 
     const { user, loading } = useSelector(state => state.auth)
+    const { cartItems } = useSelector(state => state.cart)
 
     const logoutHandler = () => {
         dispatch(logout());
@@ -39,7 +40,7 @@ const Header = () => {
                 <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
                     <Link to="/cart" style={{ textDecoration: 'none' }}>
                         <span id="cart" className="ml-3">Cart</span>
-                        <span className="ml-1" id="cart_count">2</span>
+                        <span className="ml-1" id="cart_count">{cartItems.length}</span>
                     </Link>
 
                     {user ? (
